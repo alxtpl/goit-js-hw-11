f5bccf86b5b802a0f13c487224707815afc280ad
alxtpl <alxtpl@gmail.com>
2022-01-18 22:06:23 +0200

    adding fixes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import './sass/main.scss';
 import fetchPictures from './fetchPictures';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// const lightbox = new SimpleLightbox('.gallery a', {
-//     /* options */
-//     captionsData: 'alt',
-//     captionDelay: 250,
-// });
+console.log(SimpleLightbox);
 
 const formNode = document.querySelector('.search-form');
 const divNode = document.querySelector('.gallery');
 formNode.addEventListener('submit', onSubmit);
-
 async function onSubmit(e) {
     e.preventDefault();
     console.log('inputNode', formNode.searchQuery.value);
@@ -24,30 +17,28 @@ async function onSubmit(e) {
 
 function markupFoo(data) {
     const markup = data.hits.map(item => {
-        return `<a href="${item.largeImageURL}"><div class="photo-card">
-        
+        return `<a href="${item.largeImageURL}" class="gallary-link"><div class="photo-card">
          <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-         
          <div class="info">
              <p class="info-item">
-                 <b>Likes</b>${item.likes}
+                 <b>Likes: </b>${item.likes}
              </p>
              <p class="info-item">
-                 <b>Views</b>${item.views}
+                 <b>Views: </b>${item.views}
              </p>
              <p class="info-item">
-                 <b>Comments</b>${item.comments}
+                 <b>Comments: </b>${item.comments}
              </p>
              <p class="info-item">
-                 <b>Downloads</b>${item.downloads}
+                 <b>Downloads: </b>${item.downloads}
              </p>
          </div>
      </div></a>`;
     });
     divNode.insertAdjacentHTML('afterbegin', markup.join(''));
+    const lightbox = new SimpleLightbox('.gallery a', {
+        /* options */
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
 }
-const lightbox = new SimpleLightbox('.gallery a', {
-    /* options */
-    captionsData: 'alt',
-    captionDelay: 250,
-});
