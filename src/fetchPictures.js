2fc7bc7633d3079b8f76736e136f1d1a6e1c3c06
alxtpl <alxtpl@gmail.com>
2022-01-19 03:13:49 +0200

    adding fixes

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,5 +1,7 @@
-export default async function fetchPictures(name) {
-    const baseUrl = `https://pixabay.com/api/?key=25290317-a78b6ca683460bb34341152ac&q=${name}&image_type=photo`;
+import axios from 'axios';
+
+export default async function fetchPictures(name, page) {
+    const baseUrl = `https://pixabay.com/api/?key=25290317-a78b6ca683460bb34341152ac&q=${name}&image_type=photo&per_page=40&page=${page}`;
     const responce = await fetch(baseUrl);
     if (responce.ok) {
         return responce.json();
--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
 import './sass/main.scss';
+import debounce from 'lodash.debounce';
 import fetchPictures from './fetchPictures';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(SimpleLightbox);
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// console.log(SimpleLightbox);
+let page = 1;
 const formNode = document.querySelector('.search-form');
 const divNode = document.querySelector('.gallery');
+const nextBtnNode = document.querySelector('.load-more');
+
+function clearFoo() {
+    divNode.innerHTML = '';
+}
+async function drawImages() {
+    try {
+        const data = await fetchPictures(formNode.searchQuery.value, page);
+        if (data.total === 0) {
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        } else {
+            page += 1;
+            console.dir(data);
+            markupFoo(data);
+        }
+    } catch (error) {
+        console.log('errrrrrrrrrrr');
+        Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+    // window.scroll(0, window.scrollY / 2);
+}
 formNode.addEventListener('submit', onSubmit);
-async function onSubmit(e) {
+
+function onSubmit(e) {
     e.preventDefault();
+    clearFoo();
     console.log('inputNode', formNode.searchQuery.value);
-    const data = await fetchPictures(formNode.searchQuery.value);
-    console.log(data.hits);
-    markupFoo(data);
+    drawImages();
+}
+window.addEventListener('scroll', debounce(onScroll, 300));
+
+function onScroll() {
+    // console.log('window.scrollY ', window.scrollY);
+    // console.log('window.innerHeight', window.innerHeight);
+    // console.log('document.documentElement.scrollHeight', document.documentElement.scrollHeight);
+    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+        console.log('onScroll');
+        // nextBtnNode.style.visibility = 'visible';
+        // nextBtnNode.addEventListener('click', drawImages);
+        drawImages();
+    }
 }
 
 function markupFoo(data) {
@@ -35,10 +71,11 @@ function markupFoo(data) {
          </div>
      </div></a>`;
     });
-    divNode.insertAdjacentHTML('afterbegin', markup.join(''));
+    divNode.insertAdjacentHTML('beforeend', markup.join(''));
     const lightbox = new SimpleLightbox('.gallery a', {
         /* options */
         captionsData: 'alt',
         captionDelay: 250,
     });
+    lightbox.refresh();
 }
